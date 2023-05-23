771d91698dfe2788d45bdc435f64a56198e97302
David Calaiò <davidcjr11@gmail.com>
2023-05-24 00:40:16 +0200

    add on index, need to fix

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <?php
 
-require_once __DIR__ . '/models/Products.php';
+require_once __DIR__ . '/models/categories/Food.php';
+require_once __DIR__ . '/models/categories/Toys.php';
+require_once __DIR__ . '/models/categories/Kennels.php';
+
+$cani = [
+    new Food('Cibo per cani', 'https://www.foodpet.it/wp-content/uploads/2022/02/luscious_lamb_pork_2160x.jpg', '12.99$', 'Cani', '12kg', 'Manzo'),
+    new Toys('Giochi per cani', 'https://www.petfashionstore.it/wp-content/uploads/2021/12/rew1.jpg', '3.99$', 'Cani', 'Rosso','Silicone'),
+    new Kennels('Cuccia per cani', 'https://croci.net/wp-content/uploads/2020/09/Cuccia_per_cani_Chalet.jpg', '19.99$', 'Cani', '130cm','Silicone'),
+];
+$cani[0]-> setWeight('12 kg');
+$cani[0]-> setTaste('Manzo');
+$cani[1]-> setColor('Rosso');
+$cani[1]-> setMaterials('Silicone');
+$cani[2]-> setHeight('130 cm');
+$cani[2]-> setMaterials('Legno');
+
+var_dump($cani[0]->getWeight());
+var_dump($cani[0]->getTaste());
+var_dump($cani[1]->getColor());
+var_dump($cani[1]->getMaterials());
+var_dump($cani[2]->getHeight());
+var_dump($cani[2]->getMaterials());
 
 ?>
 <!DOCTYPE html>
@@ -15,11 +36,22 @@ require_once __DIR__ . '/models/Products.php';
 <body>
     <div class="container">
         <h1 class="text-center my-5">PHP OOP 2</h1>
-
-        <div class="row">
-            <div class="col-4">
-
-            </div>
+        <span class="fs-3">Cani</span>
+        <div class="row text-center g-5">
+            <?php foreach($cani as $cane): ?>
+                <div class="col-4 mt-5 border rounded-4 p-0">
+                    <div class="fs-3"><?php echo $cane->name ?></div>
+                   <img src="<?php echo $cane->poster ?>" alt="" class="w-25">
+                   <ul style="list-style: none;" class="text-start">
+                        <li><?php echo 'Peso: ' . $cane->getWeight()?></li>
+                        <li><?php echo 'Gusto: ' . $cane->getTaste()?></li>
+                        <li><?php echo 'Colore: ' . $cane->getColor()?></li>
+                        <li><?php echo 'Materiale: ' . $cane->getMaterials()?></li>
+                        <li><?php echo 'Altezza: ' . $cane->getHeight()?></li>
+                        <li><?php echo 'Materiale: ' . $cane->getMaterials()?></li>
+                   </ul> 
+                </div>
+            <?php endforeach; ?>
         </div>
 
     </div>
--- models/Products.php
+++ models/Products.php
@@ -1,16 +1,13 @@
 <?php
 
-require_once __DIR__ . '/categories/Food.php';
-require_once __DIR__ . '/categories/Kennels.php';
-require_once __DIR__ . '/categories/Toys.php';
-
 class Products{
     public $name;
     public $poster;
     public $price;
     public $categories;
+    
 
-    function __construct($_name, $_poster, $_price, $_categories ){
+    function __construct($_name, $_poster, $_price, $_categories,){
         $this->setName($_name);
         $this->setPrice($_price);
         $this->setPoster($_poster);
