1eab93ef5cb6afe4524b82fc6a9dc98688bb4cdc
David Calaiò <davidcjr11@gmail.com>
2023-05-23 21:43:58 +0200

    fix scaffolding, add class products

--- models/Products.php
+++ models/Products.php
@@ -1,4 +1,19 @@
 <?php
 
-require_once __DIR__ . '/categories/Cats.php';
-require_once __DIR__ . '/categories/Dogs.php';
+require_once __DIR__ . '/categories/Food.php';
+require_once __DIR__ . '/categories/Kennels.php';
+require_once __DIR__ . '/categories/Toys.php';
+
+class products{
+    public $name;
+    public $poster;
+    public $price;
+    public $categories;
+
+    public function __construct($_name, $_poster, $_price, $_categories ){
+        $this->name = $_name;
+        $this->poster = $_poster;
+        $this->price = $_price;
+        $this->categories = $_categories;
+    }
+}
